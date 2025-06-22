6958f0fecd65b064eecc1704003a402328ced2be
rajibjonchhen <rajib.jonchhen@gmail.com>
2025-06-22 17:16:20 +0100

    simple practice in python

---
+++ practice.py
@@ -0,0 +1,9 @@
+print() # equvalent to \n
+print("hello world") #hello world
+print("hello", "world") #hello world ->default separated by space
+print("hello", "world", sep="-") #hello-world
+print("hello", "world", end="...") #hello world... default end with \n but can be changed using end=""
+print("hello", "world", end="*") #hello world* 
+print("apple", "banana", "mango", sep=",", end="*") #apple,banana,mango*
+print(2+3) #5
+
