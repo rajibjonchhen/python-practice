243a0f96d6875a3d896dee9f0a46d98054c9a5d7
rajibjonchhen <rajib.jonchhen@gmail.com>
2025-06-24 23:19:25 +0100

    some more operation and operator

--- practice.py
+++ practice.py
@@ -10,9 +10,101 @@ print(2 * 3) #6
 print(2 ** 3) #8 exponential 2^3
 print(2. ** 3) #8.0
 print(2. ** 3.)  #8.0
+print(4/2) # return float  2.0
 # double slash return integer regardless of type of number divided and divisor
 print(1//2) # double slash return integer 0
 print(4//2) #double slash return integer  2
 print(6//2) #double slash return integer  3
 print(7//2) #double slash return integer  3
+# when both numbers are +  - rounding always goes to the higher integer. 
+print(6. // 4) #1.0 
+print(-6. // -4) #1.0 
+# when one of a number is - rounding always goes to the lesser integer. 
+print("# when one of a number is - rounding always goes to the lesser integer.")
+print(-6 // 4) #-2
+print(6. // -4) #-2.0
 
+# remainder module %
+print("# remainder module %")
+print(5%2) #1
+print(5%3) #2
+print(14%5) #4
+print(14%4.5) #0.5
+print(12%4.5) #3.0
+
+# addition
+print("# addition")
+print(-4+4) #0
+print(-4+4.0) #0.0
+print(-4.0+8) #4.0
+print(8.0-4) #4.0
+
+# subtraction
+print("# subtraction")
+print(-4-4) #0 
+print(-4-4.0) #0.0
+print(-4.0-8) #4.0
+print(8.0-4) #4.0
+
+# multiple 
+print(2 + 3 * 5) #17
+print(9 % 6 % 2) #1 from left to right: first 9 % 6 gives 3, and then 3 % 2 gives 1;
+print(2**2**3) #256  = 2^(2^3) = 2^8 
+print(-2**3) # -8 = -2 * -2 * -2
+print(-3**2) #  9 = -3 * -3
+print(2 * 3 % 5) #
+print(-3 ** 2)
+print(-2 ** 3)
+print(-(3 ** 2))
+print((-3) ** 2)
+
+# 0. () 
+# 1. **
+# 2. +- unary next to the right of power operator bind more strongly
+# 3. / // % *
+# 4. + -
+
+print(2 * 3 % 5) #1 = 6%5
+
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
+print((5 * (12 + 100) / 26) // 2)
+print((5 * 112 / 26) // 2)
+print((5 * 4.) // 2)
+print(20. // 2)
+
+print((2 ** 4), (2 * 4.), (2 * 4))
+print(16, 8.0, 8)
+
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))
+print(-0.5, 0.5, 0, -1)
+
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))
+print(  -2,  2, 512)
+
+#['False', 'None', 'True', 'and', 'as', 'assert', 'break', 
+# 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 
+# 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 
+# 'is',  'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+# 'return', 'try', 'while', 'with', 'yield']
+
+
+#Here is a short story:
+
+# Once upon a time in Appleland, John had three apples, Mary had five apples, and Adam had six apples. They were all very happy and lived for a long time. End of story.
+
+# Your task is to:
+
+# create the variables: john, mary, and adam;
+# assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
+# having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+# now create a new variable named total_apples equal to the addition of the three previous variables.
+# print the value stored in total_apples to the console;
+# experiment with your code: create new variables, assign different values to them, and perform various arithmetic 
+# operations on them (e.g., +, -, *, /, //, etc.). Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
+
+john = 3
+mary = 5
+adam = 6
+print(john, mary, adam, sep=",")
+total_apples = john + mary + adam
+print("Total numbers of apples :", + total_apples)
