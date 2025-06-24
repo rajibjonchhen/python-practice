1b59b07d702081e2a27a8dac929dfa8c6907b08d
rajibjonchhen <rajib.jonchhen@gmail.com>
2025-06-24 18:30:29 +0100

    python // and **
    some more info about the operator in python // and **

--- practice.py
+++ practice.py
@@ -6,4 +6,13 @@ print("hello", "world", end="...") #hello world... default end with \n but can b
 print("hello", "world", end="*") #hello world* 
 print("apple", "banana", "mango", sep=",", end="*") #apple,banana,mango*
 print(2+3) #5
+print(2 * 3) #6
+print(2 ** 3) #8 exponential 2^3
+print(2. ** 3) #8.0
+print(2. ** 3.)  #8.0
+# double slash return integer regardless of type of number divided and divisor
+print(1//2) # double slash return integer 0
+print(4//2) #double slash return integer  2
+print(6//2) #double slash return integer  3
+print(7//2) #double slash return integer  3
 
